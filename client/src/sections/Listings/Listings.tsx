b25e821c38be3ffc5088f5065ed048511c4c285c
Yevgeniy <verchenkoyevgeniy@gmail.com>
2020-12-03 16:06:22 +0300

    [client hooks] init auto loading

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {server} from "../../lib/api";
-import {ListingData} from "./types";
+import {DeleteListingData, DeleteListingVariables, Listing, ListingData} from "./types";
 
 const LISTINGS = `
     query Listing {
@@ -18,19 +18,42 @@ const LISTINGS = `
     }
 `;
 
+const DELETE_LISTING = `
+    mutation DeleteListing($id: ID!) {
+        deleteListing(id: $id) {
+            id
+        }
+    }
+`;
+
 interface ListingsProps {
     title: string;
 }
 
 export const Listings = ({title}: ListingsProps) => {
 
+    const [listings, setListings] = useState<Listing[] | null>(null);
+
+    useEffect(() => {
+        fetchListings();
+    }, []);
+
     const fetchListings = async () => {
         const { data } = await server.fetch<ListingData>({query: LISTINGS});
-        console.log(data.listings);
+        setListings(data.listings);
     };
+
+    const deleteListing = async (id: string) => {
+        await server.fetch<DeleteListingData, DeleteListingVariables>({query: DELETE_LISTING, variables: { id }});
+        fetchListings();
+    };
+
+    const listingsList = listings ? <ul>{listings.map(listing =>
+            <li key={listing.id}>{listing.title} <button onClick={() => deleteListing(listing.id)}>Delete</button></li>)}
+        </ul> : null;
 
     return <div>
         <h2>{title}</h2>
-        <button onClick={fetchListings}>Query Listings</button>
+        {listingsList}
     </div>
 };
