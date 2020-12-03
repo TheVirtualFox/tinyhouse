00d7c671f93415d909b1c202f530599f20e9ebdd
Yevgeniy <verchenkoyevgeniy@gmail.com>
2020-12-03 17:08:36 +0300

    [client hooks] useMutation

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {server, useQuery} from "../../lib/api";
+import { useMutation, useQuery} from "../../lib/api";
 import {DeleteListingData, DeleteListingVariables, ListingsData} from "./types";
 
 const LISTINGS = `
@@ -33,16 +33,17 @@ interface ListingsProps {
 export const Listings = ({title}: ListingsProps) => {
 
     const {data, loading, error, refetch} = useQuery<ListingsData>(LISTINGS);
+    const [deleteListing, {loading: deleteListingLoading, error: deleteListingError}] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
 
-    const deleteListing = async (id: string) => {
-        await server.fetch<DeleteListingData, DeleteListingVariables>({query: DELETE_LISTING, variables: { id }});
+    const handleDeleteListing = async (id: string) => {
+        await deleteListing({ id });
         refetch();
     };
 
     const listings = data ? data.listings : null;
 
     const listingsList = listings ? <ul>{listings.map(listing =>
-            <li key={listing.id}>{listing.title} <button onClick={() => deleteListing(listing.id)}>Delete</button></li>)}
+            <li key={listing.id}>{listing.title} <button onClick={() => handleDeleteListing(listing.id)}>Delete</button></li>)}
         </ul> : null;
 
     if (loading) {
@@ -53,8 +54,13 @@ export const Listings = ({title}: ListingsProps) => {
         return <h2>Error</h2>
     }
 
+    const deleteListingLoadingText = deleteListingLoading ? <h4>Delete in progress...</h4> : null;
+    const deleteListingErrorText = deleteListingError ? <h4>Delete error</h4> : null;
+
     return <div>
         <h2>{title}</h2>
         {listingsList}
+        {deleteListingLoadingText}
+        {deleteListingErrorText}
     </div>
 };
