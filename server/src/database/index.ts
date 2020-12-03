2fd15769fc2b15634f681a97a4d1956cbc789415
Yevgeniy <verchenkoyevgeniy@gmail.com>
2020-12-03 09:49:39 +0300

    [server]: init env

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,13 +1,9 @@
 import {MongoClient} from 'mongodb';
+import {Database} from "../lib/types";
 
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
-// mongo_user mongo_user_fdhsafiusadhi
-const user = 'mongo_user';
-const userPassword = 'mongo_user_fdhsafiusadhi';
-const cluster = 'cluster0.kumpe';
-const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/<dbname>?retryWrites=true&w=majority`
-
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
     const client = await MongoClient.connect(url, { useNewUrlParser: true, /* useUnifiedTopology: true */ });
     const db = client.db('main');
     return {
